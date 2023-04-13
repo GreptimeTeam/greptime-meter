b2e0308563122cf5491cbcadb19758717c6742f7
fys <1113014250@qq.com>
2023-04-13 11:04:16 +0800

    fix: cargo fmt

--- cu-core/src/write_calc.rs
+++ cu-core/src/write_calc.rs
@@ -1,6 +1,6 @@
-/// Trait representing how to calculate the byte count of a write request. 
-/// 
-/// The `wcu!` macro uses this trait. See the documentation of the `cu-macros` 
+/// Trait representing how to calculate the byte count of a write request.
+///
+/// The `wcu!` macro uses this trait. See the documentation of the `cu-macros`
 /// crate for details.
 pub trait WriteCalc {
     fn byte_count(&self) -> u32;
